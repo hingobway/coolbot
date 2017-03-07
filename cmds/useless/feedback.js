4877024c30301d536184d99699e99b138f1956f0
hingobway <hingobway@users.noreply.github.com>
2017-03-06 19:17:34 -0500

    ;feedback draft 2

--- cmds/useless/feedback.js
+++ cmds/useless/feedback.js
@@ -9,30 +9,32 @@ class feedback extends disc.Command{
       memberName: 'feedback',
       description: 'Leave feedback or suggestions for the bot.'
     });
+  }
 
-    async run(message, args){
-      if(args!=''){
-        custio.init('f9c5e7cd1d2f112057a5','2c756998c4f76acafbde');
-        var d=new Date();
-        custio.identify({
-          id:message.author.id,
-          created_at: d.getTime(),
-          user: message.author.username
-        }).catch(err=>{
-          message.reply('Sorry, there was a problem with your message.');
-          console.log(err);
-        });
-        custio.track(message.author.id,'sendFeedback',{
-          umsg:args
-        })
-        .done(message.channel.sendMessage('Feedback sent.'))
-        .catch(err=>{
-          message.reply('Sorry, there was a problem with your message.');
-          console.log(err);
-        });
-      }else{
-        message.reply('You forgot your feedback.');
-      }
+  async run(message, args){
+    if(args!=''){
+      custio.init('f9c5e7cd1d2f112057a5','2c756998c4f76acafbde');
+      var d=new Date();
+      custio.identify({
+        id:message.author.id,
+        email: 'me@hingobway.com',
+        created_at: d.getTime(),
+        user: message.author.username
+      }).catch(err=>{
+        message.reply('Sorry, there was a problem with your message.');
+        console.log(err);
+      });
+      custio.track(message.author.id,'discfeedback',{
+        umsg:args
+      })
+      .done(message.channel.sendMessage('Feedback sent.'), function(err){
+        message.reply('Sorry, there was a problem with your message.');
+        console.log(err);
+      });
+    }else{
+      message.reply('You forgot your feedback.');
     }
   }
 }
+
+module.exports = feedback;
